e227e92dac74ff1ed80757be13d758d4f1960349
Prem Kumar <99128172+Premst@users.noreply.github.com>
2022-08-11 23:44:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         if(this.scroll>500){
+//             $('.scroll-up-btn').addClass("show");
+//         }else{
+//             $('.scroll-up-btn').removerClass("show")
+//         }
+//     });
+//     // slid-up script 
+//     $('.scroll-up-btn').click(function(){
+//         $('html').animate({scrollTop:0});
+//     });
+//     // typing Animation script 
+//     var typed=new Typed("typing", {
+//         strings:["Student", "Developer", "Freelancer", "Front-End Developer"],
+//         typeSpeed:100,
+//         backSpeed:60, 
+//         loop:true
+//     });
+// });
+
+// -------------about--------------
+// var tablinks = document.getElementsByClassName("tab-links")
+// var tabcontents = document.getElementsByClassName("tab-contents")
+
+// var event: Event ;
+// function opentab(tabname){
+//     for(tablink of tablinks){
+//         tablink.classList.remove("active-link");
+//     }
+//     for(tabcontent of tabcontents){
+//         tabcontent.classList.remove("active-tab");
+//     }
+//     event.currentTarget.classList.add("active-link");
+// }
